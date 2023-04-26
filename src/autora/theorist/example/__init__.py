fba793e561d6d992c7acbf2f8f7aac43e63c3a98
John Gerrard Holland <john_holland1@brown.edu>
2023-04-26 16:18:36 +0000

    add examples using equations

--- src/autora/theorist/example/__init__.py
+++ src/autora/theorist/example/__init__.py
@@ -5,4 +5,26 @@ from sklearn.base import BaseEstimator
 
 
 class ExampleRegressor(BaseEstimator):
-    pass
+    """
+    Include inline mathematics in docstring \\\\(x < 1\\\\) or $c = 3$
+    or block mathematics:
+    
+    \\\\[
+        x + 1 = 3
+    \\\\]
+
+
+    $$  
+    y + 1 = 4 
+    $$
+
+    """
+    def __init__(self):
+        pass
+
+    def fit(self, x, y):
+        pass
+
+    def predict(self, x):
+        pass
+
